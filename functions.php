a7e6233f60f9d7841cbb6a1589b0efda21d1f0c5
Felipe Lavín Z <felipe@yukei.net>
2013-08-07 22:50:39 -0400

    mejorar carga de estilos; modificacion tipografias

--- functions.php
+++ functions.php
@@ -7,6 +7,9 @@ class yukeiChild{
 		add_action('after_setup_theme', array($this, 'registerImageSizes'));
 		add_filter('post_class', array($this, 'filterPostClass'));
 		add_filter('syntaxhighlighter_themes', array($this, 'filterHighlighterThemes'));
+		add_filter('baylys_header_image_width', array($this, 'filterHeaderImgWidth'));
+		add_filter('baylys_image_height', array($this, 'filterHeaderImgHeight'));
+		add_action('wp_print_styles', array($this, 'removeRedundantStyles'), 20);
 	}
 	public static function getInstance(){
 		if ( !isset(self::$instance) ){
@@ -18,10 +21,19 @@ class yukeiChild{
 	public function __clone(){
 		trigger_error('Clone is not allowed.', E_USER_ERROR);
 	}
+	public function removeRedundantStyles(){
+		wp_dequeue_style('googleFonts');
+	}
+	public function filterHeaderImgWidth(){
+		return 1920;
+	}
+	public function filterHeaderImgHeight(){
+		return 500;
+	}
 	public function enqueueFrontStuff(){
-		// wp_enqueue_style('yukei-fonts', 'http://fonts.googleapis.com/css?family=Alegreya:900|Source+Sans+Pro:300,600,900,300italic,600italic');
-		wp_enqueue_style('yukei-fonts', 'http://fonts.googleapis.com/css?family=Alegreya:900');
+		wp_enqueue_style('yukei-fonts', 'http://fonts.googleapis.com/css?family=Lato:400,400italic,700,700italic,900');
 		wp_enqueue_style('baylys-parent-css', get_template_directory_uri() .'/style.css', array('yukei-fonts') );
+		wp_enqueue_style('yukei-style', get_stylesheet_uri(), array('baylys-parent-css', 'yukei-fonts') );
 		wp_register_style('syntaxhighlighter-theme-github', get_stylesheet_directory_uri()  .'/shThemeGitHub.css', array('syntaxhighlighter-core'));
 	}
 	public function registerImageSizes(){
